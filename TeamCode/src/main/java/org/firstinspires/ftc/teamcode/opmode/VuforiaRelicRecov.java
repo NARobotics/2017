5e937ef18839821b9a0e03ec480c3830acf7dea5
klenatown <33179245+klenatown@users.noreply.github.com>
2017-10-28 12:12:07 -0400

    Add files via upload

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/VuforiaRelicRecov.java
@@ -0,0 +1,44 @@
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.robotcore.external.navigation.VuMarkInstanceId;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+package org.firstinspires.ftc.teamcode.opmode;
+
+/**
+ * Created by WillKlena on 10/28/17.
+ * Video Tutorial
+ * https://www.youtube.com/watch?v=dVcBceqjyHw
+ */
+
+@Autonomous(name ="VuforiaRelic", group = "Test")
+public class VuforiaRelicRecov extends LinearOpMode {
+    OpenGLMatrix lastlocation = null;
+    double tX;
+    double tZ;
+    double tY;
+
+    double rX;
+    double rZ;
+    double rY;
+
+    VuforiaLocalizer  vuforia;
+
+        public void runOpMode() {
+            int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+
+        }
+}
