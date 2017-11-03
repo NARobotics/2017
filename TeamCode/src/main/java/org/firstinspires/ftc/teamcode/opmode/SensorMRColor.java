954564bddd6e86116252a91761280e0d06fc617a
klenatown <33179245+klenatown@users.noreply.github.com>
2017-11-03 13:44:55 -0400

    Update SensorMRColor.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/SensorMRColor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/SensorMRColor.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 
-@TeleOp(name = "Sensor: MR Color", group = "Sensor")
+@Autonomous(name = "Sensor: MR Color", group = "Sensor")
 public class SensorMRColor extends LinearOpMode {
 
   ColorSensor colorSensor;    // Hardware Device Object
