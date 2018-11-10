2efec99d34a5dce0e6f1732d09c7d16a3b24aa5f
Patrick McElroy <pdmcelroy1001@gmail.com>
2018-11-10 14:26:07 -0500

    Test Push of New Code
    Pushing the first few new files to Github so that we can all start working over GitHub.

--- 2017-2018 Code/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/aMainCode/CompetitionDriveCode.java
+++ 2017-2018 Code/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/aMainCode/CompetitionDriveCode.java
@@ -1,24 +1,25 @@
 package org.firstinspires.ftc.teamcode.aMainCode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 
-@TeleOp(name="GOOD DRIVE CODE", group="Iterative Opmode")
-public class MainDriveCode extends OpMode
+@TeleOp(name="COMPETITION DRIVE CODE", group="Iterative Opmode")
+public class CompetitionDriveCode extends OpMode
 {
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor rightFrontDrive, rightBackDrive, leftFrontDrive, leftBackDrive;
+    private DcMotor rightFrontDrive, rightBackDrive, leftFrontDrive, leftBackDrive, intake, lift, shooterAngle, outake;
+
     //, baseLeftArm, baseRightArm, secondLeftArm, secondRightArm;
 
-    //private CRServo rail;
+    //private CRServo buttonPusher;
 
-    private Servo clamp, rail;
+    private Servo rail;
 
     private float drivePowerRY, drivePowerRX, drivePowerLY, drivePowerLX;
     //private double xPower, yPower, liftPower, speedState;
@@ -37,20 +38,19 @@ public class MainDriveCode extends OpMode
         leftFrontDrive = hardwareMap.dcMotor.get("leftFrontDrive"); //Left drive motors
         leftBackDrive = hardwareMap.dcMotor.get("leftBackDrive");
 
+        intake = hardwareMap.dcMotor.get("intake"); //intake motor
+
+        lift = hardwareMap.dcMotor.get("lift");
+
+        shooterAngle = hardwareMap.dcMotor.get("shooterAngle");
+
+        //outake = hardwareMap.dcMotor.get("outake");
+
+
         leftFrontDrive.setDirection(DcMotorSimple.Direction.REVERSE); //Setting reverse direction to account for spin
-        rightBackDrive.setDirection(DcMotorSimple.Direction.REVERSE);
-        rightFrontDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotorSimple.Direction.REVERSE);
 
-      /*baseLeftArm = hardwareMap.dcMotor.get("baseLeftArm"); //Forward extension motor for the Base
-        baseRightArm = hardwareMap.dcMotor.get("baseRightArm"); //Backwards motor for the base
-
-        secondLeftArm = hardwareMap.dcMotor.get("secondLeftArm"); //Forward motor for the second arm
-        secondRightArm = hardwareMap.dcMotor.get("secondRightArm"); //Backwards motor for the second arm */
-
-
-        //Designating servo names
-        clamp = hardwareMap.servo.get("clamp"); //Servo for clamp
-        rail = hardwareMap.servo.get("rail"); //hook so the robot can lift itself up
+       rail = hardwareMap.servo.get("rail"); //Servo for the rail
 
 
         //</editor-fold>
@@ -71,8 +71,7 @@ public class MainDriveCode extends OpMode
     public void start() {
         runtime.reset();
 
-        clamp.setPosition(.5);
-        rail.setPosition(.5);
+
     }
     /*
      * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
@@ -100,69 +99,123 @@ public class MainDriveCode extends OpMode
         leftBackDrive.setPower(v3);
         rightBackDrive.setPower(v4);
 
+
+
+
+
+        if (gamepad2.left_stick_y == 0)
+        {
+          shooterAngle.setPower(0);
+        }
+
+        if(gamepad2.left_stick_y > 0)
+        {
+          shooterAngle.setPower(.1);
+        }
+
+        if(gamepad2.left_stick_y < 0)
+        {
+          shooterAngle.setPower(-.1);
+        }
+
+        telemetry.addData("shooterAngle", shooterAngle.getPower());
+
+
+
+
+
+        if(!gamepad2.right_bumper && !gamepad2.left_bumper)
+        {
+          intake.setPower(0);
+        }
+
+        if(gamepad2.right_bumper)
+        {
+          intake.setPower(1.5);
+        }
+
+        if(gamepad2.left_bumper)
+        {
+          intake.setPower(-0.5);
+        }
+        telemetry.addData("intake", intake.getPower());
+
+
+
+
+
+
+        if (gamepad1.left_trigger == 0 && gamepad1.right_trigger == 0)
+        {
+          lift.setPower(0);
+        }
+
+        if(gamepad1.right_trigger > 0)
+        {
+          lift.setPower(1.5);
+        }
+
+        if(gamepad1.left_trigger > 0)
+        {
+          lift.setPower(-1.5);
+        }
+        telemetry.addData("lift", lift.getPower());
+
+
+/*
+
+        if (gamepad2.left_trigger == 0 && gamepad2.right_trigger == 0)
+        {
+          outake.setPower(0);
+        }
+
+        if(gamepad2.right_trigger > 0)
+        {
+          outake.setPower(0.7);
+        }
+
+        if(gamepad2.left_trigger > 0)
+        {
+          outake.setPower(-0.7);
+        }
+        telemetry.addData("outake", outake.getPower());
+
+*/
+
+
+
+
+
+
         //</editor-fold>
 
         //<editor-fold desc="Controller 2">
-  if (gamepad2.a && clamp.getPosition() >=.03) {
+/*
+        if (gamepad2.a && clamp.getPosition() >=.03) {
             clamp.setPosition(clamp.getPosition() - .03);
 
         }
         if (gamepad2.b && clamp.getPosition() <=.97) {
             clamp.setPosition(clamp.getPosition() + .03);
         }
-
-        if (!gamepad2.x && !gamepad2.y){
-            rail.setPosition(.5);
+*/
+        if(!gamepad1.x && !gamepad1.y)
+        {
+          rail.setPosition(0.5);
         }
-        if (gamepad2.y) {
+        if (gamepad1.x){
             rail.setPosition(rail.getPosition() + .03);
         }
-        if (gamepad2.x) {
+        if (gamepad1.y) {
             rail.setPosition(rail.getPosition() - .03);
         }
 
 
-        telemetry.addData("clamp", clamp.getPosition());
+        //telemetry.addData("clamp", clamp.getPosition());
         telemetry.addData("rail", rail.getPosition());
 
-        /* if (gamepad1.left_trigger == 0 && gamepad1.right_trigger ==0)
-        {
-            baseRightArm.setPower(0);
-            baseLeftArm.setPower(0);
-        }
-        if (gamepad1.left_trigger > 0) {
-            baseRightArm.setPower(.4);
-            baseLeftArm.setPower(-.4);
-        }
-
-        if (gamepad1.right_trigger > 0) {
-            baseLeftArm.setPower(.4);
-            baseRightArm.setPower(-.4);
-        }
-
-        telemetry.addData("Base Left Arm Pow", baseLeftArm.getPower());
-        telemetry.addData("Base Right Arm Pow", baseRightArm.getPower());
-
-        if (gamepad1.right_bumper) {
-            secondLeftArm.setPower(-.4);
-            secondRightArm.setPower(.4);
-        }
-
-        if (gamepad1.left_bumper) {
-            secondLeftArm.setPower(.4);
-            secondRightArm.setPower(-.4);
-        }
-
-        if (!gamepad1.right_bumper && !gamepad2.left_bumper)
-        {
-            secondLeftArm.setPower(0);
-            secondRightArm.setPower(0);
-        }
 
 
-        telemetry.addData("Second Left Arm Pow", secondLeftArm.getPower());
-        telemetry.addData("Second Right Arm Pow", secondRightArm.getPower());
-        */
 
         //</editor-fold>
 
