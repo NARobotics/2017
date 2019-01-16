24a29b5585c7252fba281f72b68d726e717a09ba
nawilliams19 <43678292+nawilliams19@users.noreply.github.com>
2019-01-16 14:48:18 -0500

    Take a big step back and literally FUCK YOUR OWN FACE! I'm going to have to head down there and I will rain down a godly fire FUCKING storm upon you! You're going to have to call the FUCKING United Nations and get a FUCKING binding resolution to keep me from FUCKING destroying you! I am talking scorched Earth MOTHERFUCKER! I will massacre you. I WILL FUCK YOU UP!

--- 2017-2018 Code/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/aMainCode/CompetitionDriveCode.java
+++ 2017-2018 Code/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/aMainCode/CompetitionDriveCode.java
@@ -1,23 +1,25 @@
 package org.firstinspires.ftc.teamcode.aMainCode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.CRServo;
 
 
-@TeleOp(name="COMPETITION DRIVE CODE", group="Iterative Opmode")
+@TeleOp(name="Competition Drive Code", group="Iterative Opmode")
 public class CompetitionDriveCode extends OpMode
 {
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor rightFrontDrive, rightBackDrive, leftFrontDrive, leftBackDrive, intake, lift, outake, arm;
+    private DcMotor rightFrontDrive, rightBackDrive, leftFrontDrive, leftBackDrive, intake, lift, boxLift;
+
+
+    private Servo craterArm, marker, arm;
 
     private float drivePowerRY, drivePowerRX, drivePowerLY, drivePowerLX;
-
 
     @Override
     public void init() {
@@ -34,17 +36,22 @@ public class CompetitionDriveCode extends OpMode
 
         intake = hardwareMap.dcMotor.get("intake"); //intake motor
 
-        lift = hardwareMap.dcMotor.get("lift");
+        lift = hardwareMap.dcMotor.get("lift"); //lift motor
 
-        outake = hardwareMap.dcMotor.get("outake");
+        boxLift = hardwareMap.dcMotor.get("boxLift"); //motor that lifts the minerals
 
-        arm = hardwareMap.dcMotor.get("arm"); //Motor for the extending arm
+
+
 
 
         leftFrontDrive.setDirection(DcMotorSimple.Direction.REVERSE); //Setting reverse direction to account for spin
         leftBackDrive.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        arm = hardwareMap.servo.get("arm"); //Servo for the outake arm
 
+        craterArm = hardwareMap.servo.get("craterArm"); //servo for the color arm
+
+        marker = hardwareMap.servo.get("marker");//servo to drop marker into crater
 
 
         //</editor-fold>
@@ -64,14 +71,15 @@ public class CompetitionDriveCode extends OpMode
     @Override
     public void start() {
         runtime.reset();
+
+
     }
     /*
      * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
      */
 
-
     @Override
-    public void loop(){
+    public void loop() {
         //<editor-fold desc="Controller 1">
         drivePowerRY = gamepad1.right_stick_y;
         drivePowerRX = gamepad1.right_stick_x;
@@ -87,68 +95,106 @@ public class CompetitionDriveCode extends OpMode
         final double v3 = r * Math.sin(robotAngle) + rightX;
         final double v4 = r * Math.cos(robotAngle) - rightX;
 
-        //test number
-
-        if(gamepad1.left_bumper) //slo-mo mode
+//CONTROLLER 1
+{
+        if(gamepad1.right_bumper)
         {
           leftFrontDrive.setPower(v1*.3);
           rightFrontDrive.setPower(v2*.3);
           leftBackDrive.setPower(v3*.3);
           rightBackDrive.setPower(v4*.3);
         }
-
-        else //regular drive mode
+        else
         {
-          leftFrontDrive.setPower(v1);
-          rightFrontDrive.setPower(v2);
-          leftBackDrive.setPower(v3);
-          rightBackDrive.setPower(v4);
+          leftFrontDrive.setPower(2*v1);
+          rightFrontDrive.setPower(2*v2);
+          leftBackDrive.setPower(2*v3);
+          rightBackDrive.setPower(2*v4);
         }
 
-        double armPower = gamepad2.right_stick_x;
-        arm.setPower(armPower);
-
-        if(!gamepad2.right_bumper && !gamepad2.left_bumper) //move noodle arms
-        {
-          intake.setPower(0);
-        }
-        if(gamepad2.right_bumper) //move noodle arms in
-        {
-          intake.setPower(1.5);
-        }
-        if(gamepad2.left_bumper) //move noodle arms out
-        {
-          intake.setPower(-1.5);
-        }
-        telemetry.addData("intake", intake.getPower());
-
-
-        if (gamepad1.left_trigger == 0 && gamepad1.right_trigger == 0) //makes sure the lift doesn't move at the wrong time
+        if (gamepad1.left_trigger == 0 && gamepad1.right_trigger == 0)
         {
           lift.setPower(0);
         }
 
-        if(gamepad1.right_trigger > 0) //moves the lift up
+        if(gamepad1.right_trigger > 0)
         {
           lift.setPower(1.5);
         }
 
-        if(gamepad1.left_trigger > 0) //moves the lift down
+        if(gamepad1.left_trigger > 0)
         {
-          lift.setPower(-1.5);
+          lift.setPower(-3.0);
         }
         telemetry.addData("lift", lift.getPower());
+}
+
+//CONTROLLER 2
+{
+        //</editor-fold>
+
+        //<editor-fold desc="Controller 2">
+
+        if (gamepad2.left_stick_y == 0)
+        {
+          boxLift.setPower(0);
+        }
+        if(gamepad2.left_stick_y > 0)
+        {
+          boxLift.setPower(-0.3);
+        }
+        if(gamepad2.left_stick_y < 0)
+        {
+          boxLift.setPower(0.3);
+        }
+        telemetry.addData("boxLift", boxLift.getPower());
+
+        if(!gamepad2.right_bumper && !gamepad2.left_bumper)
+        {
+          intake.setPower(0);
+        }
+        if(gamepad2.right_bumper)
+        {
+          intake.setPower(1.5);
+        }
+        if(gamepad2.left_bumper)
+        {
+          intake.setPower(-0.5);
+        }
+        telemetry.addData("intake", intake.getPower());
 
 
-        telemetry.addData("outake", outake.getPower());
+        if (gamepad2.a && craterArm.getPosition() < .97)
+        {
+          craterArm.setPosition(craterArm.getPosition()+ .03);
+        }
+        if (gamepad2.b && craterArm.getPosition() > .03)
+        {
+          craterArm.setPosition(craterArm.getPosition() - .03);
+        }
+        telemetry.addData("craterArm", craterArm.getPosition());
 
+        if (!gamepad2.x && !gamepad2.y)
+        {
+          arm.setPosition(.5);
+        }
+        if (gamepad2.x)
+        {
+            arm.setPosition(-.9);
+        }
+        if (gamepad2.y)
+        {
+            arm.setPosition(.9);
+        }
 
+        telemetry.addData("arm", arm.getPosition());
 
 
         //</editor-fold>
 
         telemetry.addData("Status", "Run Time: " + runtime.toString());
         telemetry.update();
+}
     }
 
     /*
